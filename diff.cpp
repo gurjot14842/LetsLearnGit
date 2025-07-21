abfb2a157cf967d5c760a073acf9179a4eabf78e
Gurjot Singh <gurjotsinghsidhu090@gmail.com>
2025-07-21 16:31:52 +0530

    adding diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    diff = a - b;
+
+    cout << "Diff of a and b = " << diff << endl;
+
+    return 0;
+}
